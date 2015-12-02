b6cfb716c8efcdcc3104df378620f7f71beaeaac
yellow-sky <nikulin.e@gmail.com>
2015-12-02 21:38:58 +0300

    Disable debug

--- qgis-plugin/src/compulink_tools.py
+++ qgis-plugin/src/compulink_tools.py
@@ -217,8 +217,8 @@ class CompulinkToolsPlugin:
             self.iface.removeToolBarIcon(action)
 
     def add_layers_from_ngw(self):
-        import pydevd
-        pydevd.settrace('localhost', port=5566, stdoutToServer=True, stderrToServer=True, suspend=False)
+        #import pydevd
+        #pydevd.settrace('localhost', port=5566, stdoutToServer=True, stderrToServer=True, suspend=False)
 
         conn_name = PluginSettings.get_selected_ngw_connection_name()
         if not conn_name:
